072124de65d06bf52b0d8bfa87184ef3a872b921
Ganesh Iyer <lastlegion@gmail.com>
2016-02-10 12:11:00 -0500

    added new geojson benchmarks

--- bench/bench.js
+++ bench/bench.js
@@ -1,19 +1,20 @@
 
-var Benchmark = require('benchmark');
-var simplify = require('../simplify');
+var Benchmark = require("benchmark");
+var simplify = require("../simplify");
 
-var points = require('../test/fixtures/1k.json');
+var points = require("../test/fixtures/1k.geo.json");
 
-console.log('Benchmarking simplify on ' + points.length + ' points...');
+console.log("Benchmarking simplify on " + points.geometery.coordinates[0][0].length + " points...");
 
 new Benchmark.Suite()
-.add('simplify (HQ)', function() {
+.add("simplify (HQ)", function() {
     simplify(points, 1, true);
 })
-.add('simplify', function() {
+.add("simplify", function() {
     simplify(points, 1, false);
 })
-.on('cycle', function(event) {
-    console.log(String(event.target));
+.on("cycle", function(event) {
+    console.log((event.target));
+    //console.log(event);
 })
 .run();
